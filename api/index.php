7a60f84ee6e3dc1f40406120afd22f144bfedae3
Ray <rudiadriian@gmail.com>
2026-01-09 23:58:08 +0700

    Final fix: early putenv for package manifest in api index

--- api/index.php
+++ api/index.php
@@ -1,16 +1,12 @@
 <?php
 
-$paths = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions',
-    '/tmp/bootstrap/cache',
-];
+$tmpPath = '/tmp/bootstrap/cache';
 
-foreach ($paths as $path) {
-    if (!is_dir($path)) {
-        mkdir($path, 0777, true);
-    }
+if (!is_dir($tmpPath)) {
+    mkdir($tmpPath, 0777, true);
 }
+
+putenv("LARAVEL_PACKAGE_MANIFEST=$tmpPath/packages.php");
+putenv("LARAVEL_SERVICES_CACHE=$tmpPath/services.php");
 
 require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,18 +4,9 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-/*
-| Konfigurasi Khusus Vercel
-| Kita alihkan folder storage dan cache ke /tmp agar tidak Error 500
-*/
+// Jalankan ini hanya jika di server Vercel
 if (isset($_SERVER['VERCEL_URL'])) {
-    // Gunakan folder /tmp untuk storage (sessions, views, cache)
     $app->useStoragePath('/tmp/storage');
-
-    // Paksa folder bootstrap cache ke /tmp agar bisa ditulis (Writable)
-    $app->bind('path.bootstrap', function () {
-        return '/tmp/bootstrap';
-    });
 }
 
 $app->singleton(
