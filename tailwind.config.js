514f3526d450793430d3fd0da2c541021039bb28
Ray <rudiadriian@gmail.com>
2026-01-12 11:43:36 +0700

    Refactor asset web

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class', // Penting untuk tombol Dark/Light mode nanti
+    darkMode: 'class',
     content: [
       "./resources/**/*.blade.php",
       "./resources/**/*.js",
-      "./resources/**/*.vue",
     ],
     theme: {
       extend: {
         fontFamily: {
-          sans: ['Inter', 'sans-serif'], // Kita pakai font Inter agar modern
+          sans: ['Inter', 'sans-serif'],
         },
       },
     },
